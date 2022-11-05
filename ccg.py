a1336b9dc8b2ffbf92107b5a94f2beb62c90a26e
imbored3 <all19@stmarys-ca.edu>
2022-11-05 12:01:34 -0700

    just added some comments to explain stuff

--- ccg.py
+++ ccg.py
@@ -8,15 +8,21 @@ class Card:
         # card_desc: description of what the card does; if it's a draw card, discard, etc.
         self.card_desc = card_desc
 
+#positive point values, varying.
 class Innovation_Card(Card):
-    def __init__(self, name, current_point_value, card_desc):
+    def __init__(self, name, current_point_value, card_desc, cost):
         super().__init__(name, current_point_value, card_desc)
+        #cost: list of categories that will be pulled when this innovation card is drawn.
+        self.cost = cost
 
+#negatice point values, the same.
 class Environmental_Card(Card):
     pass
 
+#0 point values
 class Cleanup_Card(Card):
     def __init__(self, name, current_point_value, card_desc, category, removal):
         super().__init__(name, current_point_value, card_desc, category)
+        #removal: Specific Category of cards that this card can remove from a player's hand.
         self.removal = removal
     pass
