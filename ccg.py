af6d33005372f0f544667040ca952c5864147a5f
imbored3 <all19@stmarys-ca.edu>
2022-11-05 11:14:40 -0700

    made subclasses

--- ccg.py
+++ ccg.py
@@ -1,8 +1,22 @@
 class Card:
-    def __init__(self, point_value, kind, card_desc):
-        # point_value: how many points the card is worth
-        self.point_value = point_value
-        # kind: whether it's environmental or innovational
-        self.kind = kind
+    def __init__(self, name, current_point_value, category, card_desc):
+        self.name = name
+        # current_point_value: how many points the card is worth
+        self.current_point_value = current_point_value
+        # what type of issue does it lead to?
+        self.category = category
         # card_desc: description of what the card does; if it's a draw card, discard, etc.
         self.card_desc = card_desc
+
+class Innovation_Card(Card):
+    def __init__(self, name, current_point_value, card_desc):
+        super().__init__(name, current_point_value, card_desc)
+
+class Environmental_Card(Card):
+    pass
+
+class Cleanup_Card(Card):
+    def __init__(self, name, current_point_value, card_desc, category, removal):
+        super().__init__(name, current_point_value, card_desc, category)
+        self.removal = removal
+    pass
