387f7edd510aa55e782e9c133caf9bd5d1a612f3
Said <sm67@stmarys-ca.edu>
2022-11-05 12:25:22 -0700

    12.25pm

---
+++ enironmentalCards.py
@@ -0,0 +1,4 @@
+from ccg import Card, Environmental_Card
+
+generatingPower = Environmental_Card("Generating Power", -5, "GlobalWarming", "Description")
+
---
+++ gui.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from ccg import *
+
+root = Tk()
+root.title("Wasteful Innovation")
+root.geometry('800x600')
+root.state("zoomed")
+
+
+root.mainloop()
